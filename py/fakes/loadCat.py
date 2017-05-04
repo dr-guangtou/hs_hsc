63a5fa773d76a38f754201d9d8f5f21db9ebcd65
dr-guangtou <dr.guangtou@gmail.com>
2017-05-04 15:28:16 -0700

    Clean the data

--- py/fakes/loadCat.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-list = 'galaxy_test.lis'
-
-dat = np.loadtxt(list, dtype=[('id','int'), ('mag','float'), ('re','float'),
-                              ('ns','float'), ('ba','float'), ('pa','float')])
-
-print dat
--- py/fakes/randomSersicGenerator.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from numpy.random import uniform
-
-def drawFlatDistr():
-
-    return None
-
-
